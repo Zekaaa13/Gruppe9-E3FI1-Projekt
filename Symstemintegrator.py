3545a6170ad029f8260dcf93bedbdb243c8ee7f8
yodiichai <yodsachaibao@gmail.com>
2022-12-02 10:40:16 +0100

    add system

--- Symstemintegrator.py
+++ Symstemintegrator.py
@@ -1,3 +1,6 @@
+from unicodedata import name
+
+
 class Systemintegrator():
     def __init__(self, name, alter, jahreErfahrung):
         self.name = name
@@ -5,3 +8,18 @@ class Systemintegrator():
         self.jahreErfahrung = jahreErfahrung
     def __str__(self):
         return "" % (self.name,)
+    
+    def getName__(self, name: str):
+        pass
+    def setName__(self, name: str):
+        self.__name = name
+    def getAlter__(self, alter: int):
+        pass
+    def setAlter_(self, alter: int):
+        self.__alter = alter
+    def setJahreErfahrung__(self, jahreErfahrung: int):
+        self.__jahreErfahrung = jahreErfahrung
+    def getJahreErfahrung__():
+        pass
+    def __str__(self) -> str:
+        pass
